9fcb935fd2c972568424b4fbaea46b86e8005b8d
felixwurst <felix.wurst@gmail.com>
2021-01-15 15:20:04 +0100

    changed email for sending

--- modules/emailSender.js
+++ modules/emailSender.js
@@ -2,10 +2,19 @@ const nodemailer = require('nodemailer');
 const {passwordEmail} = require('./passwords');
 // create delivery box for email
 const transporter = nodemailer.createTransport({
-    host: "mail.coding-school.org",
+    // host: "mail.coding-school.org",
+    // port: 465,
+    // auth: {
+    //     user: "info@coding-school.org",
+    //     pass: passwordEmail()
+    // },
+    // tls: {
+    //     rejectUnauthorized: false
+    // }
+    host: "smtps.udag.de",
     port: 465,
     auth: {
-        user: "info@coding-school.org",
+        user: "info@felixwurst.de",
         pass: passwordEmail()
     },
     tls: {
@@ -17,7 +26,7 @@ const transporter = nodemailer.createTransport({
 function sendEmail(email, subject, message) {
     return new Promise((resolve, reject) => {
         const mailOption = {
-            from: 'info@coding-school.org',
+            from: 'info@felixwurst.de',
             to: email,
             subject: subject,
             text: message
